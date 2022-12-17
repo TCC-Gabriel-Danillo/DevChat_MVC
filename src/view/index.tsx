f7c3ea7c28408e82c01dff96ab499d53d86c984a
Danillo Souza <danillosouza1704@gmail.com>
2022-12-17 17:45:19 -0300

    add: adicionado hook de fontes

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 
+import { useCustomFonts } from "./hooks";
 import { Route } from "./routes";
 
 const Root = () => {
+  const isLoaded = useCustomFonts();
+
+  if (!isLoaded) return;
   return <Route />;
 };
 
