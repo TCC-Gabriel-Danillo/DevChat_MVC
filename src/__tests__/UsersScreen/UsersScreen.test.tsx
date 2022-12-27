2a02cb197703576b822126cb1cb087d83ff96805
Danillo Souza <danillosouza1704@gmail.com>
2022-12-27 07:59:34 -0300

    add: Testes da página de usuários

--- src/__tests__/UsersScreen/UsersScreen.test.tsx
+++ src/__tests__/UsersScreen/UsersScreen.test.tsx
@@ -1,11 +1,12 @@
-import { act, fireEvent } from "@testing-library/react-native";
+import { act, fireEvent, waitFor } from "@testing-library/react-native";
 import { TEST_ID } from "_/constants";
 import { UsersScreen } from "_/view/screens/UsersScreen";
 import React from "react";
 
-import { conversationMock } from "../mocks/conversationMock";
-import { createConversationMock } from "../mocks/conversationServiceStub";
-import { userAutenticatedMock } from "../mocks/userAutenticatedMock";
+import { conversationMock } from "../mocks/conversation.mock";
+import { conversationServiceStub } from "../mocks/conversationService.stub";
+import { userAutenticatedMock } from "../mocks/userAutenticated.mock";
+import { mockedParticipant, usersServiceStub } from "../mocks/usersService.stub";
 import { renderWithProviders } from "../utils/renderWithProvider";
 
 beforeEach(() => {
@@ -14,15 +15,19 @@ beforeEach(() => {
 
 describe("UsersScreen", () => {
   it("Deve renderizar usuários quando estiver logado", async () => {
-    const { findByTestId } = renderWithProviders(<UsersScreen />, {
-      preloadedState: {
-        auth: userAutenticatedMock,
-      },
-    });
+    const { store } = await waitFor(async () =>
+      renderWithProviders(<UsersScreen />, {
+        preloadedState: {
+          auth: userAutenticatedMock,
+        },
+      })
+    );
 
-    const userCard = await findByTestId(TEST_ID.USER_CARD);
+    const { users } = store.getState().users;
 
-    expect(userCard).toBeDefined();
+    expect(users).toMatchObject([mockedParticipant]);
+    expect(usersServiceStub.listUsersByTech).toBeCalled();
+    expect(usersServiceStub.listUsersByTech).toBeCalledWith("any_tech");
   });
 
   it("Deve criar conversa caso ainda não exista", async () => {
@@ -38,6 +43,6 @@ describe("UsersScreen", () => {
       fireEvent.press(userCard);
     });
 
-    expect(createConversationMock).toBeCalledWith(conversationMock);
+    expect(conversationServiceStub.createConversation).toBeCalledWith(conversationMock);
   });
 });
